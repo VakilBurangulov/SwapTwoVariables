651aff39200ae24bf386577411ff8d6a9bb70c19
Vakil <burangulov.vakil@gmail.com>
2026-02-01 12:51:23 +0300

    Добавьте файлы проекта.

---
+++ SwapTwoVariables/SwapTwoVariables.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 42;
+    int b = 153;
+    std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    std::cout << "\n";
+    std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+}
